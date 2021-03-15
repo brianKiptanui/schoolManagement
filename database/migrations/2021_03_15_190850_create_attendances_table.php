8875d49b6e0efe734bb174b725e3882c96262296
kibetbrian <kimutaibrian9@gmail.com>
2021-03-15 02:05:36 -0700

    Initial commit

---
+++ database/migrations/2021_03_15_190850_create_attendances_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->integer('student_id');
+            $table->integer('teacher_id');
+            $table->string('attendance_status');
+            $table->string('day');
+            $table->integer('lesson_id');
+            $table->integer('subject_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
