5219f7b6150a96ed1658e872d8c45d4db0a30b3c
kiptanui <kimutaibrian9@gmail.com>
2021-03-15 16:14:51 +0300

    All Changes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+     Route::post('login', 'Auth\AuthController@login');
+     Route::post('register', 'Auth\AuthController@register');
+     
+     Route::group([
+        'middleware' => 'auth:api'
+      ], function() {
+          Route::get('logout', 'Auth\AuthController@logout');
+          Route::get('user', 'Auth\AuthController@user');
+   });
+
+
+
+
